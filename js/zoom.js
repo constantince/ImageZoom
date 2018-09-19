7d5b2e4258c05e0e2f11c323fe841eedabee50b1
constantince <alberteinstein007@126.com>
2018-09-19 19:47:23 +0800

    clear some bugs

--- js/zoom.js
+++ js/zoom.js
@@ -96,13 +96,3 @@
 	window.Zoomer = start;
 
 })(window);
-
-
-Zoomer({
-	originlPanel: document.getElementById('a'),
-	targetPanel: document.getElementById('b'),
-	imageHeight: 227,
-	imageWidth: 300,
-	imageUrl: 'https://mdn.mozillademos.org/files/5397/rhino.jpg',
-	zoom: 3
-});
